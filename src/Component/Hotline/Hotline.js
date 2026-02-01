e55b60e5acd43fe81200fce94fb6799a39f56866
Qamar Yasin <qyasin303@gmail.com>
2026-02-01 23:24:22 +0500

    Initial text

--- src/Component/Hotline/Hotline.js
+++ src/Component/Hotline/Hotline.js
@@ -7,7 +7,7 @@ export const Hotline = () => {
       <h1>Emergency hotline</h1>
       <span className="con_text">(+01) – 256 567 550</span>
       <p>We provide 24/7 customer support. Please feel free to contact us
-      for emergency case....</p>
+      for emergency case.....</p>
     </div>
   )
 }
